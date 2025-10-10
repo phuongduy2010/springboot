bf3ae20fa3f92037d87f3d3b715d8ae176cc32c6
y.ly <phuongduy2010@gmail.com>
2025-10-10 11:44:41 +0700

    Init produdct service

---
+++ product-service/src/main/java/com/nhuy/productservice/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package com.nhuy.productservice.repository;
+
+import com.nhuy.productservice.model.Product;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface ProductRepository extends MongoRepository<Product, String> {
+}
